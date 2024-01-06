de2f5079f30559c6ccddfd1802fa4400beb19076
Mohammad <orabe.mhd@gmail.com>
2024-01-06 02:36:35 +0100

    Add Variablen topic

---
+++ InformathekJavaCourse/src/JavaMaterials/kapitel1/Variablen.java
@@ -0,0 +1,30 @@
+package JavaMaterials.kapitel1;
+
+public class Variablen {
+    public static void main(String[] args) {
+
+        // Create Variables in Java
+        int speedLimit = 80;
+
+        // Change values of variables
+        int limit;
+        limit = 90;
+        limit = 100;
+
+        // Rules for Naming Variables in Java
+        int age = 24;
+        int AGE = 25;
+
+        System.out.println(age);  // prints 24
+        System.out.println(AGE);  // prints 25
+
+        int alter;  // valid name and good practice
+        int _alter;  // valid but bad practice
+        int $alter;  // valid but bad practice
+
+        // invalid variables
+        // int 1age;
+        // int my age;
+
+    }
+}
